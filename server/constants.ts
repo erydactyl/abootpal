fd0b064aec7388539d5e31ac700e4dc1a2c6915a
Dylan White <dylan_j_white@yahoo.co.uk>
2019-12-27 19:12:40 +0000

    Extend Research and Judging default timers

--- server/constants.ts
+++ server/constants.ts
@@ -8,8 +8,8 @@ export const CHATMESSAGE_MAX_LENGTH = 256;
 // Timers
 export const TIMERS_DEFAULT: { [playstate: string] : number; } = {
     ChooseArticle: 10,
-    Research: 60,
-    Judging: 60, // This is number of seconds per non-judge player! eg. with 4 players total, the full phase will take 3x this time
+    Research: 90,
+    Judging: 90, // This is number of seconds per non-judge player! eg. with 4 players total, the full phase will take 3x this time
     Scores: 15
 }
 export const NUMBER_OF_ROUNDS_DEFAULT = 1;
